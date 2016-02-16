c5965db3823ed20f4cd5e6c5bbad197c5fa17665
chris-zh <chris.zh@outlook.com>
2016-02-16 22:20:13 +0800

    ''

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
-__author__ = 'chris.zhang'
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import config
+__author__ = 'chris.zhang'
+
 
 db = SQLAlchemy()
+
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -13,3 +15,5 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     return app
+
+create_app('default')
