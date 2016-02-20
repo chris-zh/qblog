7b9553c1f474d29613f51ae98be68e10b2a68e70
chris-zh <chris.zh@outlook.com>
2016-02-20 19:01:47 +0800

    day5

--- app.py
+++ app.py
@@ -14,9 +14,14 @@ def addUser(name, email, phone, password):
     user.save()
 
 
+def queryUser(name, email, phone, password):
+    user = models.T_USER.get(email=email, password=password)
+    print(user)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    return render_template('signin.html')
 
 
 @app.route('/register', methods=['POST'])
@@ -28,6 +33,20 @@ def register():
         phone = request.form['phone']
         addUser(name, email, phone, password=None)
     return render_template('user.html', username=name, message='注册成功！'),
+
+
+@app.route('/helloworld', methods=['POST', 'GET'])
+def signin():
+    email = request.form['email']
+    password = request.form['password']
+
+
+
+def post(request):
+    if request.method == 'POST':
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
